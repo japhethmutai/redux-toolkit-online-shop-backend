1c57dd116ab71a51e5c01d105f51e92bc155369c
Japheth <japhemchirie@gmail.com>
2022-02-17 20:40:07 +0300

    first commit

---
+++ products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price: 999,
+        image: "https://res.cloudinary.com/japhethmutai/image/upload/v1643876585/online-shop/iphone12pro_ulmmkn.jpg"
+    },
+    {
+        id: 2,
+        name: "iPhone 12",
+        desc: "5.4-inch display",
+        price: 699,
+        image: "https://res.cloudinary.com/japhethmutai/image/upload/v1643876585/online-shop/iphone12_ptzt9h.jpg"
+    },
+    {
+        id: 3,
+        name: "Galaxy S",
+        desc: "6.5-inch display",
+        price: 399,
+        image: "https://res.cloudinary.com/japhethmutai/image/upload/v1643876585/online-shop/galaxyS_vswjv1.png"
+    },
+];
+
+module.exports = products;
